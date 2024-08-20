ff26d75b6f1e943ed8a2e4c24c600a8a93474390
oladitisodiq <oladitisodiq@email.com>
2024-08-20 12:52:39 +0100

    docker update

--- 00-laravel-deploy.sh
+++ 00-laravel-deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Running composer"
 composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html
+composer install --no-dev --working-dir=/var/www/telegram_discord_bot
 
 echo "generating application key..."
 php artisan key:generate --show
@@ -11,3 +11,5 @@ php artisan config:cache
 
 echo "Caching routes..."
 php artisan route:cache
+
+
